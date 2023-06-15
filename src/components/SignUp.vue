cc9ef87fda35178d9d2ab078ba96b592aa621367
jurandarjanovic <darjan.juranovic@gmail.com>
2023-06-15 13:30:48 +0200

    registracija-uspjesno dodavanje novih korisnika

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,16 +1,42 @@
 <template>
-<img class="logo1" src="../assets/logo1.png">
+<img class="logo1" src="../assets/logo1.png" title="CareerLift">
 <h1>Registracija</h1>
 <div class="registracija">
-    <input type="text" placeholder="Unesite ime" />
-    <input type="email" placeholder="Unesite email" />
-    <input type="password" placeholder="Unesite lozinku" />
-    <button> Registriraj se </button>
+    <input type="text" v-model="name" placeholder="Unesite ime" />
+    <input type="email" v-model="email" placeholder="Unesite email" />
+    <input type="password" v-model="password" placeholder="Unesite lozinku" />
+    <button v-on:click="signUp"> Registriraj se </button>
 </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
-    name :'SignUp'
+    name :'SignUp',
+    data()
+    {
+        return {
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        async signUp()
+        {
+            let result = await axios.post("http://localhost:3000/korisnici",{
+               email:this.email,
+               password:this.password,
+               name:this.name
+            });
+
+            console.warn(result);
+            if(result.status==201)
+            {
+                alert("sign-up done");
+                localStorage.setItem("user-info",JSON.stringify(result.data))
+            }
+        }
+    }
 }
 </script>
 
@@ -29,6 +55,12 @@ export default {
     border: 1px solid skyblue;
 }
 .registracija button{
-    
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    background: skyblue;
+    color: white;
+    cursor: pointer;
+
 }
 </style>
