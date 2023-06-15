eb10f9f4e98ed7021929cadbcc24f1c8bffa5d82
jurandarjanovic <darjan.juranovic@gmail.com>
2023-06-15 07:32:35 +0200

    registracija

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,8 +1,20 @@
 <template>
-<h1>Registriraj se</h1>
+<img class="logo1" src="../assets/logo1.png">
+<h1>Registracija</h1>
+<div>
+    <input type="text" placeholder="Unesite ime" />
+    <input type="email" placeholder="Unesite email" />
+    <input type="text" placeholder="Unesite lozinku" />
+</div>
 </template>
 <script>
 export default {
     name :'SignUp'
 }
 </script>
+
+<style>
+.logo1{
+    width: 150px;
+}
+</style>
