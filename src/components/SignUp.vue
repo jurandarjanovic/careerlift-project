fc07096f502e74f4d4cc5d8f8facdb377c068a5d
jurandarjanovic <darjan.juranovic@gmail.com>
2023-06-15 07:55:13 +0200

    registracija-input polja

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,10 +1,11 @@
 <template>
 <img class="logo1" src="../assets/logo1.png">
 <h1>Registracija</h1>
-<div>
+<div class="registracija">
     <input type="text" placeholder="Unesite ime" />
     <input type="email" placeholder="Unesite email" />
-    <input type="text" placeholder="Unesite lozinku" />
+    <input type="password" placeholder="Unesite lozinku" />
+    <button> Registriraj se </button>
 </div>
 </template>
 <script>
@@ -16,5 +17,18 @@ export default {
 <style>
 .logo1{
     width: 150px;
+}
+.registracija input{
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid skyblue;
+}
+.registracija button{
+    
 }
 </style>
